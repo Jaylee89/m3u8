18e58242a9e2c33f62e61028d96618a4448cf1f4
Jaylee89 <121769651@qq.com>
2022-04-24 23:48:51 +0800

    code enhancement

--- movie/run.py
+++ movie/run.py
@@ -11,49 +11,67 @@ headers = {
 }
 
 
-def download_ts(url, key, i):
+def download_ts(url, key, i, tmp='tmp'):
     r = requests.get(url, headers=headers)
     data = r.content
     data = AESDecrypt(data, key=key, iv=key)
-    with open(f"tmp/{i:0>5d}.ts", "ab") as f:
+    with open(f"{tmp}/{i:0>5d}.ts", "ab") as f:
         f.write(data)
     print(f"\r{i:0>5d}.ts Downloaded", end="  ")
 
 
 def get_real_url(url):
     playlist = m3u8.load(uri=url, headers=headers)
-    return playlist.playlists[0].absolute_uri
+    return playlist.playlists[0].absolute_uri if len(playlist.playlists) else url
 
 
 def AESDecrypt(cipher_text, key, iv):
+    if key is None:
+        return cipher_text
     cipher_text = pad(data_to_pad=cipher_text, block_size=AES.block_size)
     aes = AES.new(key=key, mode=AES.MODE_CBC, iv=key)
     cipher_text = aes.decrypt(cipher_text)
     return cipher_text
 
 
-def download_m3u8_video(url, max_workers=10):
-    if not os.path.exists("tmp"):
-        os.mkdir('tmp')
+def download_m3u8_video(url, tempdir='tmp', max_workers=10):
+    # __TMP = os.path.join(os.getcwd(), 'tmp', tempdir)
+    if not os.path.exists(tempdir):
+        os.makedirs(tempdir)
 
     real_url = get_real_url(url)
     playlist = m3u8.load(uri=real_url, headers=headers)
-    key = requests.get(playlist.keys[-1].uri, headers=headers).content
+    if playlist.keys[0]:
+        key = requests.get(playlist.keys[-1].uri, headers=headers).content
+    else:
+        key = None
 
     with ThreadPoolExecutor(max_workers=max_workers) as pool:
+        total_duration=0
         for i, seg in enumerate(playlist.segments):
-            pool.submit(download_ts, seg.absolute_uri, key, i)
+            total_duration = total_duration + seg.duration
+            # pool.submit(download_ts, seg.absolute_uri, key, i, tempdir)
+        print(f"total duration is {total_duration}s")
 
-def generate_video(save_name):
+def generate_video(save_name, tempdir):
     with open(save_name, 'wb') as fw:
-        files = glob.glob('tmp/*.ts')
-        files_sorted = files.sort(reverse = False)
+        files = glob.glob(f'{tempdir}/*.ts')
+        _ = files.sort(reverse = False)
         for file in files:
+            print(f'\rread {file}\r')
             with open(file, 'rb') as fr:
                 fw.write(fr.read())
-                print(f'\r{file}Merged!Total:{len(files)}', end="     ")
+                # print(f'\r{file} Merged!Total:{len(files)}\r')
             # os.remove(file)
 
 if __name__ == "__main__":
     # _ = download_m3u8_video('https://vod8.wenshibaowenbei.com/20210628/g4yNLlI7/index.m3u8')
-    _ = generate_video('Walk into the door2.mp4')
+    # _ = generate_video('Walk into the door2.mp4')
+
+    # title
+    # _ = download_m3u8_video('https://pili-vod.hsw.cn/hswzhibo_20220422113844_574.m3u8')
+    tempdir="tmp/智慧城"
+    # m3u8, .ts file download
+    _ = download_m3u8_video('https://pili-vod.hsw.cn/hswzhibo_20220422113844_574.m3u8', tempdir=tempdir)
+    # merge all *.ts to .mp4
+    _ = generate_video('zhihuicheng.mp4', tempdir) #合成的数据可能有问题
