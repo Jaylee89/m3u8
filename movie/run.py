4e09f083e2efb18cf0a1f296c558563a3280dae5
Jaylee89 <121769651@qq.com>
2022-05-01 12:43:47 +0800

    auto cut move with same piece

--- movie/run.py
+++ movie/run.py
@@ -1,7 +1,7 @@
 import glob
 from concurrent.futures import ThreadPoolExecutor
 import m3u8
-import os
+import os, math
 import requests
 from Crypto.Util.Padding import pad
 from Crypto.Cipher import AES
@@ -53,16 +53,34 @@ def download_m3u8_video(url, tempdir='tmp', max_workers=10):
             pool.submit(download_ts, seg.absolute_uri, key, i, tempdir)
         # print(f"total duration is {total_duration}s")
 
-def generate_video(save_name, tempdir):
-    with open(save_name, 'wb') as fw:
-        files = glob.glob(f'{tempdir}/*.ts')
-        _ = files.sort(reverse = False)
-        for file in files:
-            print(f'\rread {file}\r')
-            with open(file, 'rb') as fr:
-                fw.write(fr.read())
-                # print(f'\r{file} Merged!Total:{len(files)}\r')
-            # os.remove(file)
+def generate_video(save_name, tempdir, cut_count=0):
+    files = glob.glob(f'{tempdir}/*.ts')
+    _ = files.sort(reverse = False)
+    cut_array = calculate_cut_count(files, cut_count)
+    for i, element in enumerate(cut_array):
+        with open(f"{save_name}_{i}.mp4", 'wb') as fw:
+            for file in element:
+                print(f'\rread {file}\r')
+                with open(file, 'rb') as fr:
+                    fw.write(fr.read())
+                    # print(f'\r{file} Merged!Total:{len(files)}\r')
+                # os.remove(file)
+
+def calculate_cut_count(files, cut_count=0) -> list:
+    result = []
+    data_length = len(files)
+    if cut_count!=0 and data_length>cut_count:
+        piece = math.floor(data_length/cut_count)
+        for i in range(cut_count):
+            __tmp = []
+            if i == cut_count-1:
+                __tmp = files[i*piece:]
+            else:
+                __tmp = files[i*piece:piece*(i+1)]
+            result.append(__tmp)
+    else:
+        result.append(files)
+    return result
 
 if __name__ == "__main__":
     # _ = download_m3u8_video('https://vod8.wenshibaowenbei.com/20210628/g4yNLlI7/index.m3u8')
@@ -74,4 +92,4 @@ if __name__ == "__main__":
     # m3u8, .ts file download
     _ = download_m3u8_video('https://pili-vod.hsw.cn/hswzhibo_20220422113844_574.m3u8', tempdir=tempdir)
     # merge all *.ts to .mp4
-    _ = generate_video('zhihuicheng.mp4', tempdir) #合成的数据可能有问题
+    _ = generate_video('zhihuicheng', tempdir, cut_count=3) #合成的数据可能有问题
